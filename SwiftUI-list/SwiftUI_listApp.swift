02b6e7e7104a56b835a5cf475b0de06bdd276119
Ping-Sung <76198720+Ping-Sung@users.noreply.github.com>
2025-11-25 19:02:31 -0800

    Initial Commit

---
+++ SwiftUI-list/SwiftUI_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_listApp.swift
+//  SwiftUI-list
+//
+//  Created by Kevin on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-listTests/SwiftUI_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_listTests.swift
+//  SwiftUI-listTests
+//
+//  Created by Kevin on 2025/11/25.
+//
+
+import Testing
+@testable import SwiftUI_list
+
+struct SwiftUI_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
